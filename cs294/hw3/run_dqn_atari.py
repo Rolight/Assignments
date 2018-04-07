50bfd98f95cfc6fb832b99ba6f81a1ee9e1c5162
rolight <flsnnx@gmail.com>
2018-04-06 21:44:27 -0400

    hotfix

--- cs294/hw3/run_dqn_atari.py
+++ cs294/hw3/run_dqn_atari.py
@@ -111,8 +111,8 @@ def set_global_seeds(i):
 def get_session():
     tf.reset_default_graph()
     tf_config = tf.ConfigProto(
-        inter_op_parallelism_threads=1,
-        intra_op_parallelism_threads=1)
+        inter_op_parallelism_threads=0,
+        intra_op_parallelism_threads=0)
     session = tf.Session(config=tf_config)
     print("AVAILABLE GPUS: ", get_available_gpus())
     return session
